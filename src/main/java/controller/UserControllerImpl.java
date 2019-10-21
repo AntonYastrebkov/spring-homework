185c4cc539487fc0d1949cf59c292962e53eaacf
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2019-10-21 12:58:27 +0300

    Let's begin to create Service layer

--- src/main/java/controller/UserControllerImpl.java
+++ src/main/java/controller/UserControllerImpl.java
@@ -1,47 +1,54 @@
 package controller;
 
+import dao.TaskRepository;
+import dao.UserRepository;
 import entities.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import service.UserService;
 
-@Component
-public class UserControllerImpl implements UserController {
+@Controller
+public class UserControllerImpl {
+  private final UserService userService;
 
-  @Override
-  public UserController getUserController() {
-    return new UserControllerImpl();
+  @Autowired
+  public UserControllerImpl(UserService userService) {
+    this.userService = userService;
   }
 
-  @Override
-  public void singUp() {
+  // @Override
+  public void singUp(String email, String password) {
+
 
   }
 
-  @Override
+  // @Override
   public void singIn() {
 
   }
 
-  @Override
+  // @Override
   public Task createTask() {
     return new Task();
   }
 
-  @Override
+  // @Override
   public void deleteTask(Integer taskId) {
 
   }
 
-  @Override
+  // @Override
   public void findAllUserTask(Integer userId) {
 
   }
 
-  @Override
+  // @Override
   public void markTaskComplete(Task task) {
     task.setTaskComplete(true);
   }
 
-  @Override
+  // @Override
   public void markTaskIncomplete(Task task) {
     task.setTaskComplete(false);
   }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,7 @@
 package service;
 
 public interface UserService {
+    void registerNewUser(String name, String email, String number, String password);
+
 
 }
