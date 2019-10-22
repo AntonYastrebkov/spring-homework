75a59a4e1f8b3b1730720064d9e489e4a3666869
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2019-10-22 14:31:47 +0300

    Dependencies added, aspect created

---
+++ src/main/java/aspect/SubscriptionCheck.java
@@ -0,0 +1,21 @@
+package aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SubscriptionCheck {
+    private static final String IS_SUBSCRIBED = "5ebe2294ecd0e0f08eab7690d2a6ee69";
+
+    @Pointcut("execution(public * service.TaskServiceImpl.createTask(..))")
+    public void onTaskCreate() { };
+
+    @Before("onTaskCreate()")
+    public void beforeTaskCreate(JoinPoint jp) {
+
+    }
+}
--- src/main/java/config/ApplicationConfig.java
+++ src/main/java/config/ApplicationConfig.java
@@ -2,9 +2,11 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = {"controller", "service", "dao", "entities"})
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 }
