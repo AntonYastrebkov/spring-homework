6986c594dae3912cf2654000abe7643d20aa1a30
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2019-11-04 12:37:39 +0300

    Config fixes

--- homework/src/main/java/config/WebMvcConfig.java
+++ homework/src/main/java/config/WebMvcConfig.java
@@ -1,13 +1,10 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import java.util.List;
-
-//@Configuration
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
