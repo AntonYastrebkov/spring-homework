01e6685df39b2bcde8c61efff9e0e2c504ff5e2e
Anton Ishchenko <anton_ishchenko@epam.com>
2019-11-06 17:45:56 +0300

    Covered userController with tests

--- src/test/java/integration/ControllerTest.java
+++ src/test/java/integration/ControllerTest.java
@@ -1,14 +1,18 @@
 package integration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.epam.homework.config.ApplicationConfig;
 import com.epam.homework.config.SwaggerConfig;
 import com.epam.homework.config.WebInitializer;
 import com.epam.homework.controller.TaskController;
 import com.epam.homework.controller.UserController;
+import com.epam.homework.entity.UserRole;
+import com.epam.homework.exception.UserRoleException;
 import javax.servlet.ServletContext;
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,9 +25,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ApplicationConfig.class, SwaggerConfig.class, WebInitializer.class})
@@ -39,6 +43,14 @@ public class ControllerTest {
   @Before
   public void setup() throws Exception {
     mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    mockMvc.perform(post("/user/sign-up")
+        .param("id", "1")
+        .param("name", "Anton")
+        .param("email", "email@dot.com")
+        .param("phoneNumber", "+789456123")
+        .param("password", "password")
+        .param("subscription", "secret")
+        .param("userRole", UserRole.ORDINARY_USER.name())).andExpect(status().isOk());
   }
 
   @Test
@@ -53,11 +65,62 @@ public class ControllerTest {
   @Test
   public void helloPageTest() throws Exception {
     final MvcResult mvcResult = mockMvc.perform(get("/hello"))
-        .andDo(print())
-        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(status().isOk())
         .andExpect(content().string("Hello from server!"))
         .andReturn();
     Assert.assertEquals(CONTENT_TYPE, mvcResult.getResponse().getContentType());
+  }
+
+
+  @Test
+  public void userSignUpTest() throws Exception {
+    mockMvc.perform(post("/user/sign-up")
+        .param("id", "2")
+        .param("name", "Not Anton")
+        .param("email", "email@email.com")
+        .param("phoneNumber", "+79113585555")
+        .param("password", "password")
+        .param("subscription", "asdsdas")
+        .param("userRole", UserRole.ADMIN_USER.name())).andExpect(status().isOk());
+  }
+
+
+  @Test
+  public void userSignInTest() throws Exception {
+    mockMvc.perform(post("/user/sign-in")
+        .param("email", "email@dot.com")
+        .param("password", "password"))
+        .andDo(print()).andExpect(status().isOk());
+  }
+
+  @Test
+  public void userSubscribeTest() throws Exception {
+    mockMvc.perform(post("/user/subscribe")
+        .param("userEmail", "email@dot.com")).andDo(print()).andExpect(status().isOk());
+  }
+
+  @Test
+  public void userAdminCheckerTest() throws Exception {
+    mockMvc.perform(get("/user/admin")
+        .param("id", "2")
+        .param("name", "Not Anton")
+        .param("email", "email@email.com")
+        .param("phoneNumber", "+79113585555")
+        .param("password", "password")
+        .param("subscription", "asdsdas")
+        .param("userRole", UserRole.ADMIN_USER.name())).andExpect(status().isOk());
+  }
+
+  @Test(expected = NestedServletException.class)
+  public void userNotAdminCheckerTest() throws Exception {
+    mockMvc.perform(get("/user/admin")
+        .param("id", "1")
+        .param("name", "Anton")
+        .param("email", "email@dot.com")
+        .param("phoneNumber", "+789456123")
+        .param("password", "password")
+        .param("subscription", "secret")
+        .param("userRole", UserRole.ORDINARY_USER.name()));
   }
 
 }
